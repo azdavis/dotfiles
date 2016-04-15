d11e21e3effae4337aa84863413549c52511a54a
azdavis <ariel.z.davis@icloud.com>
2016-04-14 23:06:49 -0400

    always end shell scripts with 'true'
    this ensures that when the script ends with a short-form control flow
    statement of the form [expr0] && ... [exprN], and one of [expr0]-[exprN]
    evaluated to false, the exit code of the entire shell script _is_ zero,
    as was probably intended.
    
    of course, one could add a '|| true' only to these special cases, but
    why not just do it everywhere so one doesn't have to remember to do it
    then?

--- misc/base.sh
+++ misc/base.sh
@@ -1,4 +1,5 @@
 set -euo pipefail
 IFS=$'\n\t'
 err() { echo "\e[31merror:\e[0m $1" && exit 1 }
-[[ "$USER" == 'root' ]] && err 'do not run as root' || true
+[[ "$USER" == 'root' ]] && err 'do not run as root'
+true
