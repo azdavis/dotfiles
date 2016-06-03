2f725e1aa06f47eb62f9e7625847d750f291fcdb
azdavis <ariel.z.davis@icloud.com>
2016-06-03 19:43:01 -0400

    *.sh: rename err -> abort

--- misc/base.sh
+++ misc/base.sh
@@ -1,5 +1,5 @@
-err() { echo "\e[31merror:\e[0m $1" && exit 1 }
-[[ "$USER" == 'root' ]] && err 'do not run as root'
+abort() { echo "\e[31merror:\e[0m $1" && exit 1 }
+[[ "$USER" == 'root' ]] && abort 'do not run as root'
 set -euo pipefail
 IFS=$'\n\t'
 true
