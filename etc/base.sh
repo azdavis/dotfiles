fe014c830645f7e746ef2b96167b4212bd9ba8c8
azdavis <ariel.z.davis@icloud.com>
2016-06-30 13:26:52 -0400

    *.sh: move some things around

--- etc/base.sh
+++ etc/base.sh
@@ -1,7 +1,21 @@
-abort() { echo "\e[31merror:\e[0m $1" && exit 1 }
-[[ "$USER" == "root" ]] && abort "do not run as root"
-
 set -euo pipefail
 IFS=$'\n\t'
+
+abort() {
+    echo "\e[31merror:\e[0m $1" && exit 1
+}
+
+[[ "$USER" == "root" ]] && abort "do not run as root"
+
+rm_file() {
+    [[ ! -e "$1" || -h "$1" ]] && return 0
+    echo -n "rm $1 [yn]? " && read x
+    [[ "$x" == "y" ]] && rm -rf "$1"
+}
+
+sym() {
+    rm_file "$2"
+    ln -fsn "$1" "$2"
+}
 
 true
