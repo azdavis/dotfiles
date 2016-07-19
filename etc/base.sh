121a332fe2fa46a8c6b1501558802ba25a934649
azdavis <ariel.z.davis@icloud.com>
2016-07-19 13:57:16 -0400

    *.sh: use && only when it makes sense

--- etc/base.sh
+++ etc/base.sh
@@ -2,7 +2,8 @@ set -euo pipefail
 IFS=$'\n\t'
 
 abort() {
-    echo "\e[31merror:\e[0m $1" && exit 1
+    echo "\e[31merror:\e[0m $1"
+    exit 1
 }
 
 [[ "$USER" == "root" ]] && abort "do not run as root"
@@ -12,7 +13,8 @@ remove() {
         [[ -h "$1" ]] && rm "$1"
         return 0
     fi
-    echo -n "remove $1 [yn]? " && read x
+    echo -n "remove $1 [yn]? "
+    read x
     [[ "$x" == "y" ]] && rm -rf "$1"
 }
 
