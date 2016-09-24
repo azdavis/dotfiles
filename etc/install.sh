8972f8e15cf1f071c8680c7f2ae66a95a273515d
azdavis <ariel.z.davis@icloud.com>
2016-09-24 18:04:28 -0400

    remove note

--- etc/install.sh
+++ etc/install.sh
@@ -6,20 +6,16 @@ set -o pipefail
 set -o posix
 IFS=$'\n\t'
 
-note() {
-    printf "\e[32mnote:\e[0m $1\n" 1>&2
-}
-
 dst="$HOME/.config"
 repo="azdavis/config"
 
-note "installing $repo to $dst..."
+echo "installing $repo to $dst..."
 [ -L "$dst" ] && rm "$dst"
 [ -e "$dst" ] && rm -ri "$dst"
 [ -e "$dst" ] && exit 0
 git clone -q "https://github.com/$repo" "$dst"
 chmod 700 "$dst"
 "$dst/bin/do-dotfiles"
-note "install complete"
+echo "install complete"
 
 }; main
