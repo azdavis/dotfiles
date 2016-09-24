e9f0b3c72e0523b630335c34d8168470236c06b7
azdavis <ariel.z.davis@icloud.com>
2016-09-24 18:04:05 -0400

    remove confirm

--- etc/install.sh
+++ etc/install.sh
@@ -10,23 +10,13 @@ note() {
     printf "\e[32mnote:\e[0m $1\n" 1>&2
 }
 
-confirm() {
-    local x
-    printf "\e[33mconfirm:\e[0m $1? " 1>&2
-    read x < /dev/tty
-    return $([ "$x" = y ])
-}
-
 dst="$HOME/.config"
 repo="azdavis/config"
 
 note "installing $repo to $dst..."
-if [ -e "$dst" ]; then
-    if ! confirm "remove $dst"; then
-        exit 0
-    fi
-    rm -rf "$dst"
-fi
+[ -L "$dst" ] && rm "$dst"
+[ -e "$dst" ] && rm -ri "$dst"
+[ -e "$dst" ] && exit 0
 git clone -q "https://github.com/$repo" "$dst"
 chmod 700 "$dst"
 "$dst/bin/do-dotfiles"
