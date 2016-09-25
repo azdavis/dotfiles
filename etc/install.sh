1520865105b4969d579983407092409893f1b9fe
azdavis <ariel.z.davis@icloud.com>
2016-09-24 21:44:54 -0400

    install: use some variables

--- etc/install.sh
+++ etc/install.sh
@@ -8,19 +8,21 @@ IFS=$'\n\t'
 
 dst="$HOME/.config"
 repo="azdavis/config"
+tty="/dev/tty"
+zsh="$(which zsh)"
 
 echo "installing '$repo' to '$dst'"
 [ -L "$dst" ] && rm "$dst"
-[ -e "$dst" ] && rm -ri "$dst" < /dev/tty
+[ -e "$dst" ] && rm -ri "$dst" < "$tty"
 [ -e "$dst" ] && exit 0
 git clone -q "https://github.com/$repo" "$dst"
 chmod 700 "$dst"
 
 echo "doing dotfile actions"
-"$dst/bin/do-dotfiles" < /dev/tty
+"$dst/bin/do-dotfiles" < "$tty"
 
-echo "changing shell to /bin/zsh"
-chsh -s /bin/zsh < /dev/tty
+echo "changing shell to $zsh"
+chsh -s "$zsh" < "$tty"
 
 echo "all done"
 
