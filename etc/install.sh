fa81fddca1ad683e70f2421a3e0d43afcf10562f
azdavis <ariel.z.davis@icloud.com>
2016-09-30 17:25:44 -0400

    do not use $tty

--- etc/install.sh
+++ etc/install.sh
@@ -6,23 +6,22 @@ set -o pipefail
 set -o posix
 IFS=$'\n\t'
 
-dst="$HOME/.config"
 repo="azdavis/config"
-tty="/dev/tty"
+dst="$HOME/.config"
 zsh="$(which zsh)"
 
 echo "installing '$repo' to '$dst'"
 [ -L "$dst" ] && rm "$dst"
-[ -e "$dst" ] && rm -ri "$dst" < "$tty"
+[ -e "$dst" ] && rm -ri "$dst" < /dev/tty
 [ -e "$dst" ] && exit 0
 git clone -q "https://github.com/$repo" "$dst"
 chmod 700 "$dst"
 
 echo "doing dotfile actions"
-"$dst/bin/do-dotfiles" < "$tty"
+"$dst/bin/do-dotfiles" < /dev/tty
 
 echo "changing shell to $zsh"
-chsh -s "$zsh" < "$tty"
+chsh -s "$zsh" < /dev/tty
 
 echo "all done"
 
