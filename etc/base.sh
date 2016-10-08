202dbf611344d8f1f85d20b74d6397aae23880e3
azdavis <ariel.z.davis@icloud.com>
2016-10-07 20:12:04 -0400

    use symlink

--- etc/base.sh
+++ etc/base.sh
@@ -7,3 +7,11 @@ abort() {
     echo "$1"
     exit 1
 }
+
+symlink() {
+    [ -L "$2" ] && rm "$2"
+    [ -e "$2" ] && rm -ri "$2"
+    [ -e "$2" ] && return
+    mkdir -p "$(dirname "$2")"
+    ln -s "$1" "$2"
+}
