11b35d16d5b0b635a97008b78a71b3f20fa32c3f
azdavis <ariel.z.davis@icloud.com>
2017-01-13 02:20:44 -0500

    switch some order

--- install.sh
+++ install.sh
@@ -20,14 +20,14 @@ main() {
     repo="https://github.com/azdavis/dotfiles"
     install_dir="$HOME/.config"
     install_dir_git="$install_dir/.git"
-    tmp_dir="$(mktemp -d)"
     new_shell="$(which zsh)"
 
     note "installing '$repo' to '$install_dir'"
     if ! [ -e "$install_dir_git" ]; then
-        rm -rf "$install_dir_git"
-        mkdir -p "$install_dir"
+        tmp_dir="$(mktemp -d)"
         git clone "$repo" "$tmp_dir"
+        mkdir -p "$install_dir"
+        rm -rf "$install_dir_git"
         mv "$tmp_dir/.git" "$install_dir_git"
         rm -rf "$tmp_dir"
         git -C "$install_dir" reset -q --hard
