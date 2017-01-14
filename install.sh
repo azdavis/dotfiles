17fd0b226970ce6dbefd1ce9423750739f7fcdb2
azdavis <ariel.z.davis@icloud.com>
2017-01-14 01:18:38 -0500

    change some var name

--- install.sh
+++ install.sh
@@ -17,28 +17,28 @@ main() {
     need_cmd "zsh"
 
     repo="https://github.com/azdavis/dotfiles"
-    dst="$HOME/.config"
-    dst_git="$dst/.git"
+    dst_d="$HOME/.config"
+    dst_d_git="$dst_d/.git"
     new_shell="$(which zsh)"
 
-    note "installing '$repo' to '$dst'"
-    if [ ! -e "$dst_git" ] \
-    || ! git -C "$dst" rev-parse >/dev/null 2>&1 \
-    || [ "$(git -C "$dst" config remote.origin.url)" != "$repo" ]; then
+    note "installing '$repo' to '$dst_d'"
+    if [ ! -e "$dst_d_git" ] \
+    || ! git -C "$dst_d" rev-parse >/dev/null 2>&1 \
+    || [ "$(git -C "$dst_d" config remote.origin.url)" != "$repo" ]; then
         ok=true
-        tmp="$(mktemp -d)"
-        trap "rm -rf '$tmp'" EXIT
-        git clone "$repo" "$tmp"
-        if [ -e "$dst" ]; then
+        tmp_d="$(mktemp -d)"
+        trap "rm -rf '$tmp_d'" EXIT
+        git clone "$repo" "$tmp_d"
+        if [ -e "$dst_d" ]; then
             ok=false
         fi
         if $ok; then
-            mkdir -p "$dst"
-            rm -rf "$dst_git"
-            mv "$tmp/.git" "$dst_git"
-            git -C "$dst" reset -q --hard
+            mkdir -p "$dst_d"
+            rm -rf "$dst_d_git"
+            mv "$tmp_d/.git" "$dst_d_git"
+            git -C "$dst_d" reset -q --hard
             note "doing dotfile actions"
-            "$dst/bin/do-dotfiles" < /dev/tty
+            "$dst_d/bin/do-dotfiles" < /dev/tty
         fi
     fi
 
