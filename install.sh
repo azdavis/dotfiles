762304c382f7ad979ac6f93d9790f4a21e10e869
azdavis <ariel.z.davis@icloud.com>
2017-01-14 22:11:13 -0500

    move and rename some vars

--- install.sh
+++ install.sh
@@ -6,27 +6,25 @@ main() {
     set -o errexit
     set -o nounset
 
-    ok=true
+    have_all_cmds=true
     for x in cat chsh comm git ls mkdir mktemp mv rm sort wc zsh; do
         if ! which "$x" >/dev/null 2>&1; then
             note "fatal: command not found: $x"
-            ok=false
+            have_all_cmds=false
         fi
     done
-    if ! $ok; then
+    if ! $have_all_cmds; then
         exit 1
     fi
 
     repo="https://github.com/azdavis/dotfiles"
     dst_d="$HOME/.config"
     dst_d_git="$dst_d/.git"
-    new_shell="$(which zsh)"
-
     note "installing '$repo' to '$dst_d'"
     if [ ! -e "$dst_d_git" ] \
     || ! git -C "$dst_d" rev-parse >/dev/null 2>&1 \
     || [ "$(git -C "$dst_d" config remote.origin.url)" != "$repo" ]; then
-        ok=true
+        should_install=true
         tmp_d="$(mktemp -d)"
         tmp_f1="$(mktemp)"
         tmp_f2="$(mktemp)"
@@ -38,18 +36,18 @@ main() {
             ( echo .git; git -C "$tmp_d" ls-tree --name-only @ ) | sort > "$tmp_f2"
             comm -12 "$tmp_f1" "$tmp_f2" > "$tmp_f3"
             if [ "$(cat "$tmp_f3" | wc -l)" -gt 0 ]; then
-                ok=false
+                should_install=false
                 note "the following files in '$dst_d' would be overwritten:"
                 cat "$tmp_f3"
                 note "continue [yn]?"
                 printf ">>> "
                 read x < /dev/tty
                 if [ "$x" = y ]; then
-                    ok=true
+                    should_install=true
                 fi
             fi
         fi
-        if $ok; then
+        if $should_install; then
             mkdir -p "$dst_d"
             rm -rf "$dst_d_git"
             mv "$tmp_d/.git" "$dst_d_git"
@@ -59,6 +57,7 @@ main() {
         fi
     fi
 
+    new_shell="$(which zsh)"
     note "changing \$SHELL to '$new_shell'"
     if [ "$SHELL" != "$new_shell" ]; then
         chsh -s "$new_shell" < /dev/tty
