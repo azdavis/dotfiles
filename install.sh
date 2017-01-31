8f2668bca5e2847fedf54f8ffa65045948951029
azdavis <ariel.z.davis@icloud.com>
2017-01-31 13:12:48 -0500

    restructure install a bit

--- install.sh
+++ install.sh
@@ -24,7 +24,6 @@ main() {
     if [ ! -e "$dst_d_git" ] \
     || ! git -C "$dst_d" rev-parse >/dev/null 2>&1 \
     || [ "$(git -C "$dst_d" config remote.origin.url)" != "$repo" ]; then
-        should_install=true
         tmp_d="$(mktemp -d)"
         tmp_f1="$(mktemp)"
         tmp_f2="$(mktemp)"
@@ -40,26 +39,24 @@ main() {
             ) | sort > "$tmp_f2"
             comm -12 "$tmp_f1" "$tmp_f2" > "$tmp_f3"
             if [ "$(cat "$tmp_f3" | wc -l)" -gt 0 ]; then
-                should_install=false
                 note "the following items in '$dst_d' would be replaced:"
                 cat "$tmp_f3"
                 note "continue [yn]?"
                 printf ">>> "
                 read x < /dev/tty
-                if [ "$x" = y ]; then
-                    should_install=true
+                if [ "$x" != y ]; then
+                    exit
                 fi
             fi
         fi
-        if $should_install; then
-            mkdir -p "$dst_d"
-            rm -rf "$dst_d_git"
-            mv "$tmp_d/.git" "$dst_d_git"
-            git -C "$dst_d" reset -q --hard
-            note "doing home actions"
-            "$dst_d/bin/do-home" < /dev/tty
-        fi
+        mkdir -p "$dst_d"
+        rm -rf "$dst_d_git"
+        mv "$tmp_d/.git" "$dst_d_git"
+        git -C "$dst_d" reset -q --hard
     fi
+
+    note "doing home actions"
+    "$dst_d/bin/do-home" < /dev/tty
 
     new_shell="$(which zsh)"
     note "changing \$SHELL to '$new_shell'"
