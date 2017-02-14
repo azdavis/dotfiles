86aef58cbaf5f41cd792c538cddb43d7b460f66b
azdavis <ariel.z.davis@icloud.com>
2017-02-14 17:33:47 -0500

    change an error message

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@ check_for_commands() {
     have_all=true
     for x in cat chsh comm git ls mkdir mktemp mv rm sort wc zsh; do
         if ! which "$x" > /dev/null 2>&1; then
-            echo "command not found: $x"
+            echo "'$x' not installed"
             have_all=false
         fi
     done
