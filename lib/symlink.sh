ae41303eeea5000f4c75eaca2b03d5389633d515
Ariel Davis <ariel.z.davis@icloud.com>
2017-03-17 23:06:06 -0400

    do not ask

--- lib/symlink.sh
+++ lib/symlink.sh
@@ -1,15 +1,5 @@
 symlink() {
-    if [ -L "$2" ]; then
-        rm "$2"
-    fi
-    if [ -e "$2" ]; then
-        printf "rm '%s' [yn]? " "$2"
-        read x
-        if [ "$x" != y ]; then
-            return
-        fi
-        rm -rf "$2"
-    fi
+    rm -rf "$2"
     mkdir -p "$(dirname "$2")"
     ln -s "$1" "$2"
 }
