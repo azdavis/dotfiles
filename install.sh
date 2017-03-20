88c6bb7cd5e70e1a92d91dae6db5c8797f6a409a
Ariel Davis <ariel.z.davis@icloud.com>
2017-03-20 02:05:06 -0400

    do not do-subl

--- install.sh
+++ install.sh
@@ -61,11 +61,6 @@ do_home() {
     "$dst/bin/do-home"
 }
 
-do_subl() {
-    echo "doing subl actions"
-    "$dst/bin/do-subl"
-}
-
 change_shell() {
     new_shell="$(which zsh)"
     echo "changing shell to '$new_shell'"
@@ -85,7 +80,6 @@ main() {
     check_deps
     install_repo
     do_home
-    do_subl
     change_shell
     echo "finishing"
 }
