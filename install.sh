4cfaa139896293aa3ac81af8a566b6c31925d379
Ariel Davis <ariel.z.davis@icloud.com>
2017-04-16 17:49:43 -0400

    give path for mktemp

--- install.sh
+++ install.sh
@@ -43,7 +43,7 @@ install_repo() {
     && [ "$(git -C "$dst" config remote.origin.url)" = "$url" ]; then
         return
     fi
-    tmp="$(mktemp -d)"
+    tmp="$(mktemp -d /tmp/XXXXXXXXXX)"
     trap "rm -rf '$tmp'" EXIT
     git clone -q -n --single-branch "$url" "$tmp"
     if ! [ -d "$dst" ]; then
