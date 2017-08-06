766715c217a3d6213d6a726b17d150c7dcd4da88
Ariel Davis <ariel.z.davis@icloud.com>
2017-08-05 22:42:14 -0400

    do not use TMPDIR
    /tmp is POSIX.

--- install.sh
+++ install.sh
@@ -46,7 +46,7 @@ install_repo() {
 	&& [ "$(git -C "$dst" config remote.origin.url)" = "$url" ]; then
 		return
 	fi
-	tmp="$(mktemp -d "${TMPDIR:-/tmp}/XXXXXXXXXX")"
+	tmp="$(mktemp -d /tmp/XXXXXXXXXX)"
 	trap "rm -rf '$tmp'" EXIT
 	git clone -q -n --single-branch "$url" "$tmp"
 	if ! [ -d "$dst" ]; then
