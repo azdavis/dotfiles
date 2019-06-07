a29e2ecda1a06d1cc084660421a740c750a718a1
Ariel Davis <ariel.z.davis@icloud.com>
2019-06-06 22:53:26 -0700

    Rm some echo

--- install.sh
+++ install.sh
@@ -10,7 +10,6 @@ set -eu
 url="https://github.com/azdavis/dotfiles.git"
 dst="$HOME/.config"
 
-echo "finding dependencies"
 if [ "$(uname)" = Darwin ] && ! xcode-select -p >/dev/null; then
   panic "'Command Line Developer Tools' not found"
 fi
@@ -20,7 +19,6 @@ for x in /bin/sh chsh git; do
   fi
 done
 
-echo "preparing '$dst'"
 if ! [ -d "$dst" ]; then
   rm -f "$dst"
   mkdir "$dst"
@@ -28,7 +26,6 @@ fi
 cd "$dst"
 chmod 700 .
 
-echo "installing '$url'"
 if [ "$(git config remote.origin.url)" != "$url" ]; then
   rm -rf .git
   git init -q
@@ -38,10 +35,8 @@ if [ "$(git config remote.origin.url)" != "$url" ]; then
   git reset -q --hard origin/master
 fi
 
-echo "doing home actions"
 "$dst/bin/do-home" </dev/tty
 
-echo "changing shell to zsh"
 new_shell="$(grep '/zsh$' /etc/shells | head -n 1)"
 if [ -z "$new_shell" ]; then
   echo "error: zsh is not an allowed shell"
