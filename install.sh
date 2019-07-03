e28a141191b95349521395a2fd782f1dca6a9bf9
Ariel Davis <ariel.z.davis@icloud.com>
2019-07-02 22:00:24 -0700

    Use tabs

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 panic() {
-  echo "$1" >&2
-  exit 1
+	echo "$1" >&2
+	exit 1
 }
 
 main() {
@@ -11,37 +11,37 @@ url="https://github.com/azdavis/dotfiles.git"
 dst="$HOME/.config"
 
 if [ "$(uname)" = Darwin ] && ! xcode-select -p >/dev/null; then
-  panic "'Command Line Developer Tools' not found"
+	panic "'Command Line Developer Tools' not found"
 fi
 for x in /bin/sh chsh git; do
-  if ! command -v "$x" >/dev/null; then
-    panic "'$x' not found"
-  fi
+	if ! command -v "$x" >/dev/null; then
+		panic "'$x' not found"
+	fi
 done
 
 if ! [ -d "$dst" ]; then
-  rm -f "$dst"
-  mkdir "$dst"
+	rm -f "$dst"
+	mkdir "$dst"
 fi
 cd "$dst"
 chmod 700 .
 
 if [ "$(git config remote.origin.url)" != "$url" ]; then
-  rm -rf .git
-  git init -q
-  git config remote.origin.url "$url"
-  git config remote.origin.fetch "+refs/heads/*:refs/remotes/origin/*"
-  git fetch -q origin refs/heads/master:refs/remotes/origin/master
-  git reset -q --hard origin/master
+	rm -rf .git
+	git init -q
+	git config remote.origin.url "$url"
+	git config remote.origin.fetch "+refs/heads/*:refs/remotes/origin/*"
+	git fetch -q origin refs/heads/master:refs/remotes/origin/master
+	git reset -q --hard origin/master
 fi
 
 "$dst/bin/do-home" </dev/tty
 
 new_shell="$(grep '/zsh$' /etc/shells | head -n 1)"
 if [ -z "$new_shell" ]; then
-  echo "error: zsh is not an allowed shell"
+	echo "error: zsh is not an allowed shell"
 elif [ "$SHELL" != "$new_shell" ]; then
-  chsh -s "$new_shell" </dev/tty
+	chsh -s "$new_shell" </dev/tty
 fi
 
 echo "done."
