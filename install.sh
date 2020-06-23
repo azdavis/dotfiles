199ec74b92272b6498a5e401b322c002c9c8d501
Ariel Davis <ariel.z.davis@icloud.com>
2020-06-22 17:31:08 -0700

    Clean up install

--- install.sh
+++ install.sh
@@ -1,49 +1,40 @@
+set -eu
+
 panic() {
   echo "$1" >&2
   exit 1
 }
 
-main() {
-
-set -eu
-
-url="https://github.com/azdavis/dotfiles.git"
-dst="$HOME/.config"
-
-if [ "$(uname)" = Darwin ] && ! xcode-select -p >/dev/null; then
-  panic "'Command Line Developer Tools' not found"
-fi
-for x in /bin/sh chsh git; do
-  if ! command -v "$x" >/dev/null; then
-    panic "'$x' not found"
+run() {
+  url="https://github.com/azdavis/dotfiles.git"
+  dst="$HOME/.config"
+  if [ "$(uname)" = Darwin ] && ! xcode-select -p >/dev/null; then
+    panic "'Command Line Developer Tools' not found"
   fi
-done
+  for x in /bin/sh /bin/zsh chsh git; do
+    if ! command -v "$x" >/dev/null; then
+      panic "'$x' not found"
+    fi
+  done
+  if ! [ -d "$dst" ]; then
+    rm -f "$dst"
+    mkdir "$dst"
+  fi
+  cd "$dst"
+  chmod 700 .
+  if [ "$(git config remote.origin.url)" != "$url" ]; then
+    rm -rf .git
+    git init -q
+    git config remote.origin.url "$url"
+    git config remote.origin.fetch "+refs/heads/*:refs/remotes/origin/*"
+    git fetch -q origin refs/heads/master:refs/remotes/origin/master
+    git reset -q --hard refs/remotes/origin/master
+  fi
+  "$dst/bin/do-home" </dev/tty
+  if [ "$SHELL" != /bin/zsh ]; then
+    chsh -s /bin/zsh </dev/tty
+  fi
+  echo "done."
+}
 
-if ! [ -d "$dst" ]; then
-  rm -f "$dst"
-  mkdir "$dst"
-fi
-cd "$dst"
-chmod 700 .
-
-if [ "$(git config remote.origin.url)" != "$url" ]; then
-  rm -rf .git
-  git init -q
-  git config remote.origin.url "$url"
-  git config remote.origin.fetch "+refs/heads/*:refs/remotes/origin/*"
-  git fetch -q origin refs/heads/master:refs/remotes/origin/master
-  git reset -q --hard refs/remotes/origin/master
-fi
-
-"$dst/bin/do-home" </dev/tty
-
-new_shell="$(grep '/zsh$' /etc/shells | head -n 1)"
-if [ -z "$new_shell" ]; then
-  echo "error: zsh is not an allowed shell"
-elif [ "$SHELL" != "$new_shell" ]; then
-  chsh -s "$new_shell" </dev/tty
-fi
-
-echo "done."
-
-}; main
+run
